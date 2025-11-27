6ec2592bc1a7fd6fe71ccf64d0384b5e6a1a422d
Ammar <optofutureprep@gmail.com>
2025-11-27 13:51:22 -0800

    Update exam-modals-react.jsx

--- components/exam-modals-react.jsx
+++ components/exam-modals-react.jsx
@@ -291,9 +291,6 @@ const ExamModalsManager = () => {
 
     return (
         <>
-            {/* TestDataManager - handles InstantDB operations for test system */}
-            {typeof window.TestDataManager !== 'undefined' && React.createElement(window.TestDataManager)}
-            
             {showExitModal && (
                 <ExitTestModal
                     onReturn={() => setShowExitModal(false)}
